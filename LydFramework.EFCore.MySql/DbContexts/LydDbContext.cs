344f72d0ae311e1fd0c30e4037e9559d3fdf0486
LiYaDong <206977438@qq.com>
2023-04-21 17:40:26 +0800

    1

---
+++ LydFramework.EFCore.MySql/DbContexts/LydDbContext.cs
@@ -0,0 +1,33 @@
+﻿using LydFramework.Domain.Menus;
+using LydFramework.Domain.Roles;
+using LydFramework.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LydFramework.EFCore.MySql.DbContexts
+{
+    public class LydDbContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public LydDbContext(DbContextOptions<LydDbContext> options) : base(options)
+        {
+
+        }
+        public LydDbContext()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //从当前程序集加载数据库字段配置
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+    }
+}
